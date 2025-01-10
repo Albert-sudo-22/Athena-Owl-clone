57489f890c3f0ec1ade7999f3236323d72859beb
Albert <krm9346@autuni.ac.nz>
2025-01-10 16:38:09 +0700

    update side-nav

--- src/app/general-menu/general-menu.component.ts
+++ src/app/general-menu/general-menu.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-general-menu',
-  imports: [],
+  imports: [MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './general-menu.component.html',
   styleUrl: './general-menu.component.scss'
 })
 export class GeneralMenuComponent {
 
-  constructor(private router: Router){}
-
-  goToHome(): void{
-    this.router.navigate(['/'])
-  }
 }
