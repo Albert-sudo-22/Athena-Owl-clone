478a83fbcd689669053f4328c438a493c3932a7b
Albert <krm9346@autuni.ac.nz>
2025-01-13 13:54:27 +0700

    small update with item 1 in general menu

---
+++ src/app/item1/item1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item1',
+  imports: [],
+  templateUrl: './item1.component.html',
+  styleUrl: './item1.component.scss'
+})
+export class Item1Component {
+
+}
